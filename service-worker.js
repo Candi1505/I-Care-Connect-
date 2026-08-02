ec509baf112fccf6a64b8139d6dd55bda05a257f
Noob <45gnw2mmw9@privaterelay.appleid.com>
2026-08-02 14:14:20 +1000

    Publish regular medication section

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="florence-shell-20260802-7";
-const SHELL=["./","./index.html","./styles.css?v=20260801-1","./config.js","./app.js?v=20260802-1","./operations.js?v=20260802-1","./staff-management.js?v=20260801-1","./setup-code-display.js?v=20260802-1","./portal-participant-label.js?v=20260802-1","./push-notifications.js?v=20260802-2","./portal-care-plan.js?v=20260802-1","./medication-prn-fix.js?v=20260802-1","./sil.html","./sil.css?v=20260731-1","./sil.js?v=20260801-4","./manifest.webmanifest","./florence-icon.svg"];
+const CACHE="florence-shell-20260802-8";
+const SHELL=["./","./index.html","./styles.css?v=20260801-1","./config.js","./app.js?v=20260802-1","./operations.js?v=20260802-1","./staff-management.js?v=20260801-1","./setup-code-display.js?v=20260802-1","./portal-participant-label.js?v=20260802-1","./push-notifications.js?v=20260802-2","./portal-care-plan.js?v=20260802-1","./medication-prn-fix.js?v=20260802-1","./regular-medication-tab.js?v=20260802-1","./sil.html","./sil.css?v=20260731-1","./sil.js?v=20260801-4","./manifest.webmanifest","./florence-icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 async function prepareResponse(request,response){
@@ -12,6 +12,7 @@ async function prepareResponse(request,response){
   if(!html.includes("push-notifications.js"))html=html.replace("</body>",'<script src="push-notifications.js?v=20260802-2"></script></body>');
   if(!html.includes("portal-care-plan.js"))html=html.replace("</body>",'<script src="portal-care-plan.js?v=20260802-1"></script></body>');
   if(!html.includes("medication-prn-fix.js"))html=html.replace("</body>",'<script src="medication-prn-fix.js?v=20260802-1"></script></body>');
+  if(!html.includes("regular-medication-tab.js"))html=html.replace("</body>",'<script src="regular-medication-tab.js?v=20260802-1"></script></body>');
   return new Response(html,{status:response.status,statusText:response.statusText,headers:response.headers});
  }
  return response;
