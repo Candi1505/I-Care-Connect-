7dae54f883cf2058ba1942d8cbbaa06e06f928f5
Noob <45gnw2mmw9@privaterelay.appleid.com>
2026-08-04 18:11:04 +1000

    Publish reliable Florence invoice controls

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE="florence-shell-20260804-10";
+const CACHE="florence-shell-20260804-11";
 const CORE_FIX="./core-ui-fixes-v3.js?v=20260804-1";
 const DEPUTY_UI="./deputy-integration.js?v=20260804-3";
-const INVOICE_UI="./invoicing-workspace.js?v=20260804-8";
-const INVOICE_LOADER="./invoice-menu-fix.js?v=20260804-6";
+const INVOICE_UI="./invoicing-workspace.js?v=20260804-10";
+const INVOICE_LOADER="./invoice-menu-fix.js?v=20260804-10";
 const EVELYN_INVOICE_PRESETS="./evelyn-invoice-presets.js?v=20260804-1";
 const SHELL=["./","./index.html","./styles.css?v=20260801-1","./config.js?v=20260804-5","./app.js?v=20260802-1","./operations.js?v=20260802-1","./staff-management.js?v=20260801-1","./setup-code-display.js?v=20260802-4","./live-refresh-controls.js?v=20260802-2","./notification-navigation.js?v=20260802-2",CORE_FIX,DEPUTY_UI,INVOICE_UI,INVOICE_LOADER,EVELYN_INVOICE_PRESETS,"./core-ui-fixes-v2.js?v=20260804-1","./portal-participant-label.js?v=20260802-2","./portal-care-plan.js?v=20260803-2","./medication-prn-fix.js?v=20260804-2","./regular-medication-tab.js?v=20260804-5","./florence-readiness-controls.js?v=20260802-1","./remote-s8-verification.js?v=20260802-1","./sil.html","./sil.css?v=20260731-1","./sil.js?v=20260801-4","./manifest.webmanifest","./florence-icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
